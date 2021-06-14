0ec39547b48d808e295ca0eb7347027d8c63adad
Jason Bock <jason.r.bock@outlook.com>
2021-06-13 21:10:32 -0500

    Got the first test to pass!

---
+++ src/Transpire.Tests/FindNewGuidViaConstructorAnalyzerTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+using NUnit.Framework;
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Transpire.Tests
+{
+	public static class FindNewGuidViaConstructorAnalyzerTests
+	{
+		// AnalyzeWhenNoGuidIsMade
+		// AnalyzeWhenGuidIsMadeViaGuidNewGuid
+		// AnalyzeWhenGuidIsMadeViaString
+		// AnalyzeWhenGuidIsMadeViaTargetTypeNew (Guid id = new())
+
+		[Test]
+		public static async Task AnalyzeWhenGuidIsMadeViaNoArgumentConstructorAsync()
+		{
+			var code = "var id = new System.Guid();";
+			var diagnostics = await FindNewGuidViaConstructorAnalyzerTests.GetDiagnosticsAsync(code);
+
+			Assert.That(diagnostics.Length, Is.EqualTo(1));
+		}
+
+		private static async Task<ImmutableArray<Diagnostic>> GetDiagnosticsAsync(string source)
+		{
+			var syntaxTree = CSharpSyntaxTree.ParseText(source);
+			var references = AppDomain.CurrentDomain.GetAssemblies()
+				.Where(_ => !_.IsDynamic && !string.IsNullOrWhiteSpace(_.Location))
+				.Select(_ => MetadataReference.CreateFromFile(_.Location));
+			var compilation = CSharpCompilation.Create("analyzer", new SyntaxTree[] { syntaxTree },
+				references, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary))
+				.WithAnalyzers(ImmutableArray.Create(new FindNewGuidViaConstructorAnalyzer() as DiagnosticAnalyzer));
+
+			return await compilation.GetAnalyzerDiagnosticsAsync();
+		}
+	}
+}
--- src/Transpire/FindNewGuidViaConstructorAnalyzer.cs
+++ src/Transpire/FindNewGuidViaConstructorAnalyzer.cs
@@ -3,6 +3,8 @@ using Microsoft.CodeAnalysis;
 using System.Collections.Immutable;
 using Transpire.Descriptors;
 using System;
+using System.Linq;
+using Microsoft.CodeAnalysis.Operations;
 
 namespace Transpire
 {
@@ -10,16 +12,34 @@ namespace Transpire
 	public sealed class FindNewGuidViaConstructorAnalyzer
 		: DiagnosticAnalyzer
 	{
-		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics =>
-			ImmutableArray.Create(CallingNewGuidDescriptor.Create());
+		private static readonly DiagnosticDescriptor rule = CallingNewGuidDescriptor.Create();
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(FindNewGuidViaConstructorAnalyzer.rule);
 
 		public override void Initialize(AnalysisContext context)
 		{
 			context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
 			context.EnableConcurrentExecution();
-			context.RegisterOperationAction(FindNewGuidViaConstructorAnalyzer.AnalyzeConstructorInvocation, OperationKind.Invocation);
+			//context.RegisterSymbolAction(FindNewGuidViaConstructorAnalyzer.AnalyzeSymbolInvocation, SymbolKind.Method);
+			context.RegisterOperationAction(FindNewGuidViaConstructorAnalyzer.AnalyzeOperationAction, OperationKind.ObjectCreation);
 		}
 
-		private static void AnalyzeConstructorInvocation(OperationAnalysisContext obj) => throw new NotImplementedException();
+		private static void AnalyzeOperationAction(OperationAnalysisContext context)
+		{
+			var contextInvocation = ((IObjectCreationOperation)context.Operation).Constructor;
+			var guidSymbol = context.Compilation.GetTypeByMetadataName(typeof(Guid).FullName)!
+				.InstanceConstructors.Single(_ => _.Parameters.Length == 0);
+
+			if(SymbolEqualityComparer.Default.Equals(contextInvocation, guidSymbol))
+			{
+				var diagnostic = Diagnostic.Create(FindNewGuidViaConstructorAnalyzer.rule, context.ContainingSymbol.Locations[0]);
+				context.ReportDiagnostic(diagnostic);
+			}
+		}
+
+		private static void AnalyzeSymbolInvocation(SymbolAnalysisContext context) 
+		{
+			var guidSymbol = context.Compilation.GetTypeByMetadataName(typeof(Guid).FullName);
+		}
 	}
 }
