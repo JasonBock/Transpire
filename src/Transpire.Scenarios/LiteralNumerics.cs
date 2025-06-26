583bb0e18662c9d98d9692322875384fff05457a
Jason Bock <jason.r.bock@outlook.com>
2025-06-26 07:53:20 -0500

    #22 time for a merge

--- src/Transpire.Scenarios/LiteralNumerics.cs
+++ src/Transpire.Scenarios/LiteralNumerics.cs
@@ -4,5 +4,5 @@ public static class LiteralNumerics
 {
 	public const int IntValueWithSeparator = 1_23;
 	public const int IntValueWithNoSeparatorSmall = 123;
-	public const int IntValueWithNoSeparatorLarge = 3133;
+	public const int IntValueWithNoSeparatorLarge = 33333133;
 }
