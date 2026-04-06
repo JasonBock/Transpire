2fd8427c34a0b9755d0f9dad8ec0a35b4c6c2608
Jason Bock <jason.r.bock@outlook.com>
2026-04-06 07:37:46 -0500

    #44 added more equality analyzer tests

--- src/Transpire.Analysis.Tests/Analyzers/EqualityAnalyzerTests.cs
+++ src/Transpire.Analysis.Tests/Analyzers/EqualityAnalyzerTests.cs
@@ -66,4 +66,69 @@ internal static class EqualityAnalyzerTests
 			.WithSpan(7, 22, 7, 33);
 		await TestAssistants.RunAnalyzerAsync<EqualityAnalyzer>(code, [diagnostic]);
 	}
+
+	[Test]
+	public static async Task AnalyzeWhenOrderedExistsAndEqualityDoesNotUsingPropertyAsync()
+	{
+		var code =
+			"""
+			using Transpire;
+			using System;
+
+			#nullable enable
+			
+			public partial record Customer(
+				Guid Id, uint Age)
+			{
+				[Ordered(3u)] 
+				public string? Name { get; init; }
+			}
+			""";
+
+		var diagnostic = new DiagnosticResult(DescriptorIdentifiers.ExcludedOrOrderedUsedWithoutEqualityId, DiagnosticSeverity.Error)
+			.WithSpan(9, 3, 9, 14);
+		await TestAssistants.RunAnalyzerAsync<EqualityAnalyzer>(code, [diagnostic]);
+	}
+
+	[Test]
+	public static async Task AnalyzeWhenExcludedExistsAndEqualityDoesNotUsingConstructorAsync()
+	{
+		var code =
+			"""
+			using Transpire;
+			using System;
+
+			#nullable enable
+			
+			public partial record Customer(
+				Guid Id, [property: Excluded] string Name, uint Age);
+			""";
+
+		var diagnostic = new DiagnosticResult(DescriptorIdentifiers.ExcludedOrOrderedUsedWithoutEqualityId, DiagnosticSeverity.Error)
+			.WithSpan(7, 22, 7, 30);
+		await TestAssistants.RunAnalyzerAsync<EqualityAnalyzer>(code, [diagnostic]);
+	}
+
+	[Test]
+	public static async Task AnalyzeWhenExcludedExistsAndEqualityDoesNotUsingPropertyAsync()
+	{
+		var code =
+			"""
+			using Transpire;
+			using System;
+
+			#nullable enable
+			
+			public partial record Customer(
+				Guid Id, uint Age)
+			{
+				[Excluded]
+				public string? Name { get; init; }
+			}
+			""";
+
+		var diagnostic = new DiagnosticResult(DescriptorIdentifiers.ExcludedOrOrderedUsedWithoutEqualityId, DiagnosticSeverity.Error)
+			.WithSpan(9, 3, 9, 11);
+		await TestAssistants.RunAnalyzerAsync<EqualityAnalyzer>(code, [diagnostic]);
+	}
 }
